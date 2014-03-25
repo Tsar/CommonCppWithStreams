eb3901d1dfa29de147e0af6e90fdde04deea4d12
Ioann Volkov <volkov.ioann@gmail.com>
2014-03-25 19:05:35 +0400

    Fix test

--- tests/test03.cpp
+++ tests/test03.cpp
@@ -2,10 +2,12 @@ int abc = (((6773)));
 
 signed int g();
 
-unsigned long f(short int x, void y = 3, long z = 5) {
+unsigned long f(short int x, int y = 3, long z = 5, signed i = 10) {
 }
 
 int main() {
+    long y;
+    int z = 0;
     int x = y = f(50, 30, 10, z);
     return (900900);
 }
