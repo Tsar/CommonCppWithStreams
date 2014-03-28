5978aafe0725624e7156db1f6cace384e71b8b6a
Ioann Volkov <volkov.ioann@gmail.com>
2014-03-28 15:31:50 +0400

    Add an interesting test

---
+++ tests/test05.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main() {
+    int a = 14;
+    int b = 16;
+    
+    if (a == 14)
+        if (b == 15)
+            std::cout << "#1" << std::endl;
+        else
+        {
+            std::cout << "#2" << std::endl;
+        }
+//        else
+//            std::cout << "#3" << std::endl;
+    
+    ;
+    ;
+    ;
+
+    if (2 == 2);
+    
+    return 0;
+    
+    do {
+        std::cout << "a" << std::endl;
+    } while (4 == 4);
+
+    while (a);
+    
+    do;
+    while (true);
+    
+    do
+        a = 10;
+    while (true);
+    
+    {
+        a = 15;
+        int b = 6;
+    }
+    
+    if (a == 12);
+    else;
+}
