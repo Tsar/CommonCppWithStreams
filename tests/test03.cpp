17bcd0295df781b4b50343540648f763199ec493
Ioann Volkov <volkov.ioann@gmail.com>
2014-04-08 12:12:31 +0400

    Add a test from supervisor

---
+++ tests/test03.cpp
@@ -0,0 +1,8 @@
+int main(int print, int args) {
+  int print() { return print(main()); }
+  bool x = print << 2;
+  print << x << main >> x;
+  x = print() + x * x / a;;;;; //
+  if ( if(x)) print else { else x };
+  for (;;) for (a <<= 2 + a() *() / x; while (1) do x=a; a++) {{{{}}}{}{}}
+}
