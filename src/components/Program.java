468f567c4cecdd83f5d075a5652504f0eb93bc65
Ioann Volkov <volkov.ioann@gmail.com>
2014-04-10 14:04:15 +0400

    Improve 'Program'

--- src/components/Program.java
+++ src/components/Program.java
@@ -11,16 +11,24 @@ public class Program {
 	
 	public Program(Tree tree) {
 		contents = new ArrayList<VarDefOrFunction>();
-		
-		for (int i = 0; i < tree.getChildCount(); ++i) {
-        	Tree child = tree.getChild(i);
-        	if (child.getType() == CommonCppWithStreamsLexer.VAR_DEF) {
-        		contents.add(new VarDefOrFunction(new VarDef(child)));
-        	} else if (child.getType() == CommonCppWithStreamsLexer.FUNCTION) {
-        		contents.add(new VarDefOrFunction(new Function(child)));
-        	} else {
-        		assert(false);
-        	}
-        }
+
+		if (tree == null) {
+			// Nothing to do: empty program
+		} else if (tree.getType() == CommonCppWithStreamsLexer.VAR_DEF) {
+			contents.add(new VarDefOrFunction(new VarDef(tree)));
+		} else if (tree.getType() == CommonCppWithStreamsLexer.FUNCTION) {
+			contents.add(new VarDefOrFunction(new Function(tree)));
+		} else {
+			for (int i = 0; i < tree.getChildCount(); ++i) {
+	        	Tree child = tree.getChild(i);
+	        	if (child.getType() == CommonCppWithStreamsLexer.VAR_DEF) {
+	        		contents.add(new VarDefOrFunction(new VarDef(child)));
+	        	} else if (child.getType() == CommonCppWithStreamsLexer.FUNCTION) {
+	        		contents.add(new VarDefOrFunction(new Function(child)));
+	        	} else {
+	        		assert(false);
+	        	}
+	        }
+		}
 	}
 }
